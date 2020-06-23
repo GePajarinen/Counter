03a34384807abfb1c1c87f52e0ab1855e5370aa4
Gesiane Pajarinen <gesiane.pajarinen@yahoo.com>
2020-06-23 13:17:10 +0000

    save DELETE

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,7 +16,9 @@ class Counter extends Component{
         <button 
         onClick= {this.handleIncrement} 
         className= "btn btn-secondary btn-sm"> Increment</button>
-        <button className="btn btn-danger btn-sm m-2"> Delete</button>
+        <button 
+        onClick={() => this.props.onDelete(this.props.id) }
+        className="btn btn-danger btn-sm m-2"> Delete</button>
       </div>
     ); 
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,10 +11,21 @@ class Counters extends Component{
     ]
   };
 
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c=> c.id !== counterId);
+    this.setState({counters});
+  }
+
   render(){
     return (
       <div>
-        {this.state.counters.map(counter => (<Counter key={counter.id} value={counter.value} id={counter.id}/>))}
+        {this.state.counters.map(counter => (
+          <Counter 
+          key={counter.id}
+          onDelete={this.handleDelete} 
+          value={counter.value} 
+          id={counter.id}
+          />))}
     </div>);
   }
 
