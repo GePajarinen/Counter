73edef114f2fa2d7433833a3df178adc50c1775e
Gesiane Pajarinen <gesiane.pajarinen@yahoo.com>
2020-06-23 12:36:43 +0000

    cleaning the tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,30 +2,21 @@ import React, {Component} from 'react';
 
 class Counter extends Component{
   state = {
-    value: this.props.value,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
   
   handleIncrement = () => {
     this.setState({value: this.state.value + 1});
   };
 
-  renderTags(){
-    if (this.state.tags.length === 0) return <p>there are no tags!</p>;
-    
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-  }
-
   render(){
     return (
       <div>
         <span className={this.getBadgeClasses()}> {this.formatCount()} </span> 
-        
-        <button onClick= {()=> this.handleIncrement(/*product*/)}className= "btn btn-secondary btn-sm"> Increment</button>
+        <button 
+        onClick= {this.handleIncrement} 
+        className= "btn btn-secondary btn-sm"> Increment</button>
         <button className="btn btn-danger btn-sm m-2"> Delete</button>
-
-        {this.renderTags()}
-
       </div>
     ); 
   }
